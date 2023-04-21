1ccc78ee325c5dcda710d9bd60bf0a636fa10f27
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-04-21 20:39:34 +0300

    для уроку

---
+++ 02-friends.js
@@ -0,0 +1,30 @@
+/*
+ * Работа З колекцією (масивом об'єктів)
+ */
+const friends = [
+  { name: 'Mango', online: false },
+  { name: 'Kiwi', online: true },
+  { name: 'Poly', online: false },
+  { name: 'Ajax', online: false },
+];
+
+console.table(friends);
+
+/*
+ * Шукаєм друга по імені
+ */
+const findFriendByName = function (allFriends, friendName) {};
+
+// console.log(findFriendByName(friends, 'Poly'));
+// console.log(findFriendByName(friends, 'Chelsy'));
+
+/*
+ * Отримуєм імена всіх друзів
+ */
+const getAllNames = function (allFriends) {};
+
+// console.log(getAllNames(friends));
+
+/*
+ * Отримуєм імена тільки друзів які онлайн
+ */
